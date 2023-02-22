4dc041f6ea45f5481e333eedf5cdef5c3ccb50fd
Lucky-Star-999 <theboost1305@gmail.com>
2023-02-22 15:21:21 +0700

    Checkpoint

--- practice/practice-week-8-3/practice-week-8-3-submission/src/test/java/com/example/practiceweek83submission/service/impl/TutorialServiceImplTest.java
+++ practice/practice-week-8-3/practice-week-8-3-submission/src/test/java/com/example/practiceweek83submission/service/impl/TutorialServiceImplTest.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -33,11 +35,27 @@ class TutorialServiceImplTest {
 
     @Test
     void findAll() {
+        List<Tutorial> tutorials = new ArrayList<>();
+        tutorials.add(new Tutorial(1l, "Test tutorial 1", "This is a test tutorial 1", true));
+        tutorials.add(new Tutorial(2l, "Test tutorial 2", "This is a test tutorial 2", false));
 
+        when(tutorialRepository.findAll()).thenReturn(tutorials);
+
+        List<Tutorial> returnedTutorials = tutorialService.findAll();
+
+        assertEquals(2, returnedTutorials.size());
+        assertEquals("Test tutorial 1", returnedTutorials.get(0).getTitle());
+        assertEquals("This is a test tutorial 2", returnedTutorials.get(1).getDescription());
     }
 
     @Test
     void findByTitleContaining() {
+        List<Tutorial> tutorials = new ArrayList<>();
+        tutorials.add(new Tutorial(1l, "Test tutorial 1", "This is a test tutorial 1", true));
+
+        when(tutorialRepository.findByTitleContainingIgnoreCase("this")).thenReturn(tutorials);
+
+        assertThat(tutorialService.findByTitleContaining("this")).isNotNull();
     }
 
     @Test
@@ -55,13 +73,27 @@ class TutorialServiceImplTest {
 
     @Test
     void deleteAll() {
+        tutorialService.deleteAll();
+        verify(tutorialRepository).deleteAll();
     }
 
     @Test
     void findByPublished() {
+        List<Tutorial> tutorials = new ArrayList<>();
+        tutorials.add(new Tutorial(1l, "Test tutorial 1", "This is a test tutorial 1", true));
+
+        when(tutorialRepository.findByPublished(true)).thenReturn(tutorials);
+        List<Tutorial> returnedTutorials = tutorialService.findByPublished(true);
+
+        assertThat(returnedTutorials.get(0).getId()).isEqualTo(1l);
     }
 
     @Test
     void saveTutorial() {
+        Tutorial tutorial = new Tutorial(1l, "Test tutorial 1", "This is a test tutorial 1", true);
+
+        when(tutorialRepository.save(tutorial)).thenReturn(tutorial);
+
+        assertThat(tutorialService.saveTutorial(tutorial)).isNotNull();
     }
 }
